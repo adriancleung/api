c329fe602ef6b874f7bf0ce1028fa6b7903f5549
adriancleung <adrian.leung@ualberta.ca>
2020-08-30 17:20:20 -0600

    Added gmail send functionality

--- app.js
+++ app.js
@@ -1,8 +1,83 @@
+const fs = require('fs');
+const readline = require('readline');
 const { google } = require('googleapis');
 const express = require('express');
 const morgan = require('morgan');
-const gmail = google.gmail('v1');
 const app = express();
+
+const SCOPES = [
+    'https://www.googleapis.com/auth/gmail.send'
+];
+
+const TOKEN_PATH = 'token.json';
+const CREDENTIALS_PATH = 'credentials.json';
+
+fs.writeFile(CREDENTIALS_PATH, process.env.credentials_json, (err) => {
+    if (err) return console.error(err);
+    console.log('Credentials stored to', CREDENTIALS_PATH);
+})
+
+fs.writeFile(TOKEN_PATH, process.env.token_json, (err) => {
+    if (err) return console.error(err);
+    console.log('Token stored to', TOKEN_PATH);
+})
+
+function authorize(credentials, callback) {
+    const { client_secret, client_id, redirect_uris } = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) return getNewToken(oAuth2Client, callback);
+        oAuth2Client.setCredentials(JSON.parse(token));
+        callback(oAuth2Client);
+    });
+}
+
+function getNewToken(oAuth2Client, callback) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) return console.error('Error retrieving access token', err);
+            oAuth2Client.setCredentials(token);
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                if (err) return console.error(err);
+                console.log('Token stored to', TOKEN_PATH);
+            });
+            callback(oAuth2Client);
+        });
+    });
+}
+
+function sendMail(auth) {
+    from = "From: Adrian Leung <leung.c.adrian@gmail.com>\n";
+    to = "To: Adrian Leung <leung.c.adrian@gmail.com>\n";
+    subject = "Subject: Contact Form Submitted!\n\n";
+    body = "TEST";
+    encoded = encode(from + to + subject + body);
+    
+    const gmail = google.gmail({version: "v1", auth});
+    gmail.users.messages.send({
+        requestBody: {
+            raw: encoded,
+        },
+        userId: "me",
+    });
+}
+
+function encode(unencoded) {
+    var encoded = new Buffer.from(unencoded).toString('base64');
+    return encoded.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/g, '');
+};
 
 app.use(morgan('dev'));
 
@@ -10,7 +85,11 @@ app.get('/', (req, res) => {
     res.send('Hello world!');
 });
 
-app.get('/send', (req, res) => {
+app.post('/send', (req, res) => {
+    fs.readFile('credentials.json', (err, content) => {
+        if (err) return console.log('Error loading client secret file:', err);
+        authorize(JSON.parse(content), sendMail);
+    });
     res.send('Email sent');
 });
 
